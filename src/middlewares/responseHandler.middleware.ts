4371124ae5ae78fd738e2b7a815b35d2d0ec64a7
ohmprakash1506 <ohmprakash96@outlook.com>
2023-06-16 14:04:06 +0530

    responseHandler added in middleware

--- src/middlewares/responseHandler.middleware.ts
+++ src/middlewares/responseHandler.middleware.ts
@@ -1,1 +1,34 @@
 import { ApiServiceRespone } from "../@types/apiServiceResponse";
+
+const logError = (error :any) => {
+    console.error(error)
+}
+
+const returnError = (statusCode: number, message: string) => {
+    const response: ApiServiceRespone = {
+        statusCode,
+        response: {
+            status: false,
+            code: statusCode,
+            message
+        }
+    }
+}
+
+const retrunSuccess = (statusCode: number, message: string, data?: [] | object) => {
+    const response: ApiServiceRespone = {
+        statusCode,
+        response: {
+            status: true,
+            code: statusCode,
+            message,
+            data,
+        }
+    }
+}
+
+export default{
+    retrunSuccess,
+    returnError,
+    logError
+}
